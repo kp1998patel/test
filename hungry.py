deb1dec3b2d0850fb4561f31e5cb896629e50c01
Kevin Patel <patel1998kevin@gmail.com>
2020-06-23 11:17:04 +0530

    ADDED WATER

--- hungry.py
+++ hungry.py
@@ -2,4 +2,6 @@ hungry = input {"are you hungry"}
 if hungry == "yes"
     print("eat samosa")
  else:
-    print("do ur work")
+    thirsty=input("are you thirsty")
+    if thirsty=="yes":
+        print("drink water")
